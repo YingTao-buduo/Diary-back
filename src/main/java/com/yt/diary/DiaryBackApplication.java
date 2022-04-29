635d81f563d41112d6f49ad48f95eac5b6be4a06
YingTao <yingtaoyt@outlook.com>
2022-04-29 09:34:05 +0800

    Initial commit

---
+++ src/main/java/com/yt/diary/DiaryBackApplication.java
@@ -0,0 +1,13 @@
+package com.yt.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yt/diary/DiaryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yt.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
